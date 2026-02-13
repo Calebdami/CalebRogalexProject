361bd02a1c41bde3affde8e0e8f3050581f84f55
Caleb KPANOU <caleb.kpanou@chem.loc>
2026-02-13 11:44:43 +0100

    Initialisation_of_ Project

---
+++ ApplicationCalendar/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+    import CalendarView from './views/CalendarView.vue';
+    import SignInView from './views/SignInView.vue';
+</script>
+
+<template>
+    <div>
+        <SignInView/>
+    </div>
+    <div>
+        <CalendarView/>
+    </div>
+</template>
+
+<style scoped></style>
---
+++ ApplicationCalendar/src/views/CalendarView.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+
+</script>
+
+
+<template>
+    <div>
+        <header>
+        </header>
+        <main>
+        </main>
+    </div>
+</template>
+
+
+<style scoped>
+
+
+</style>
